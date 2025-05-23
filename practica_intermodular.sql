4f6693c5fc6b95063c211b13281bfff6d5210d14
Haixiao69 <hwa0001@alu.medac.es>
2025-05-23 10:42:54 +0200

    Update practica_intermodular.sql

--- practica_intermodular.sql
+++ practica_intermodular.sql
@@ -40,7 +40,6 @@ CREATE TABLE planes (
         ON DELETE CASCADE
 );
 
-
 -- Delimitador temporal para los triggers
 DELIMITER //
 
@@ -69,43 +68,52 @@ DELIMITER ;
 
 -- Insercion de datos en Categorias
 INSERT INTO categorias (nombre, descripcion) VALUES
-('Streaming', 'Plataformas de transmision de video'),
-('Musica', 'Plataformas de transmision de musica'),
+('Streaming', 'Plataformas de transmisión de video'),
+('Música', 'Plataformas de transmisión de música'),
 ('Entretenimiento', 'Plataformas de contenido variado'),
 ('Juegos', 'Plataformas de videojuegos'),
 ('Lectura', 'Plataformas de libros y lectura digital');
 
 -- Insercion de datos en Plataformas
 INSERT INTO plataformas (id_categoria, nombre, descripcion, fundadores, sitio_web) VALUES
-(1, 'Netflix', 'Plataforma de streaming de pel�culas y series', 'Reed Hastings, Marc Randolph', 'https://www.netflix.com'),
+(1, 'Netflix', 'Plataforma de streaming de películas y series', 'Reed Hastings, Marc Randolph', 'https://www.netflix.com'),
 (1, 'Disney+', 'Plataforma de streaming de Disney, Pixar, Marvel y mas', 'The Walt Disney Company', 'https://www.disneyplus.com'),
 (2, 'Spotify', 'Plataforma de streaming de musica', 'Daniel Ek, Martin Lorentzon', 'https://www.spotify.com'),
 (2, 'Apple Music', 'Servicio de streaming de musica de Apple', 'Apple Inc.', 'https://music.apple.com'),
 (3, 'YouTube', 'Plataforma de videos en linea', 'Steve Chen, Chad Hurley, Jawed Karim', 'https://www.youtube.com'),
-(4, 'Brawl Stars', 'Juego movil de Supercell', 'Supercell', 'https://supercell.com/en/games/brawlstars/'),
+(3, 'Twitch', 'Plataforma de streaming en linea', 'Justin Kan', 'https://www.twitch.tv/'),
+(4, 'Geometry Dash', 'Juego multiplataforma', 'Robert Topala', 'https://play.google.com/store/apps/details?id=com.robtopx.geometryjump&hl=es&pli=1'),
+(4, 'Brawl Stars', 'Juego de movil', 'Supercell', 'https://supercell.com/en/games/brawlstars/'),
+(5, 'Kobo', 'Plataforma de libros electronicos', 'Michael Serbinis', 'https://www.kobo.com/es/es/plus?utm_source=google&utm_medium=cpc&utm_campaign=brd_es_kplu&gad_source=1&gad_campaignid=21424108705&gbraid=0AAAAADyJFG3swq-PmeWFdM6wrVbSIYNO-&gclid=Cj0KCQjwucDBBhDxARIsANqFdr1R4kxNyxS14Awzb_kCGOsZV5WiA1IEWo36YtqEEmqCGNm940mcxZIaAtIPEALw_wcB'), 
 (5, 'Kindle', 'Plataforma de libros electronicos de Amazon', 'Amazon', 'https://www.amazon.com/kindle');
 
--- Insercion de planes para las plataformas existentes
+-- Inserción de planes para las plataformas existentes
 INSERT INTO planes (id_plataforma, nombre_plan, precio, dispositivos_maximos) VALUES
-
 -- Netflix
-(1, 'Netflix Basico', 7.99, 1),
-(1, 'Netflix Estandar', 12.99, 2),
+(1, 'Netflix Básico', 7.99, 1),
+(1, 'Netflix Estándar', 12.99, 2),
 (1, 'Netflix Premium', 17.99, 4),
 -- Disney+
-(1, 'Disney+ Estandar', 8.99, 2),
-(1, 'Disney+ Premium', 11.99, 4),
+(2, 'Disney+ Estandar', 8.99, 2),
+(2, 'Disney+ Premium', 11.99, 4),
 -- Spotify
-(2, 'Spotify Free', 0.00, 1),
-(2, 'Spotify Premium', 9.99, 3),
+(3, 'Spotify Free', 0.00, 1),
+(3, 'Spotify Premium', 9.99, 3),
 -- Apple Music
-(2, 'Apple Music Individual', 10.99, 1),
-(2, 'Apple Music Familiar', 16.99, 6),
+(4, 'Apple Music Individual', 10.99, 1),
+(4, 'Apple Music Familiar', 16.99, 6),
 -- YouTube
-(3, 'YouTube Gratuito', 0.00, 1),
-(3, 'YouTube Premium', 11.99, 2),
--- Brawl Stars (ejemplo simbolico, no tiene suscripciones tipicas)
-(4, 'Acceso Gratuito', 0.00, 1),
+(5, 'YouTube Gratuito', 0.00, 1),
+(5, 'YouTube Premium', 11.99, 2),
+-- Twitch
+(6, 'Twitch Gratuito', 0.00, 1),
+-- Geometry Dash (no tiene suscripciones típicas)
+(7, 'Acceso Completo', 2.99, 1),
+-- Brawl Stars
+(8, 'Acceso Gratuito', 0.00, 1),
+-- Kobo
+(9, 'Kobo Reading', 0.00, 1),
+(9, 'Kobo Plus', 9.99, 1),
 -- Kindle
-(5, 'Kindle Reading', 0, 1),
-(5, 'Kindle Unlimited', 19.99, 1);
+(10, 'Kindle Reading', 0.00, 1),
+(10, 'Kindle Unlimited', 9.99, 1);
